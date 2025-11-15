96169d29432f348e1a66160f8e95c78123d4c544
Tresor101 <103023224+Tresor101@users.noreply.github.com>
2025-11-15 14:58:06 +0800

    Delete Login authentication directory

--- Login authentication/Js/login.js
+++
@@ -1,9 +0,0 @@
-function verification(){
-    const userEmail = document.getElementById('userEmail1').value;
-    const password = document.getElementById('password1').value;
-    if (userEmail == password){
-        alert("LogIn Successfuln");
-    }else{
-        alert("Unsuccessful LogIn");
-    }
-} 
